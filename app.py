c074f0fcdfc276df6ad8f87854a5fe1203126eb3
James Fordemwalt <jfordemwalt@clearwateranalytics.com>
2024-12-17 16:21:17 -0700

    Refactor and add stuff

--- app.py
+++ app.py
@@ -1,162 +1,138 @@
 from flask import Flask, request, send_from_directory, render_template
-from skyrimPotions import getPotions
-from madlibs import madlib, calendar_altText, calendar_facts, country_song, whip_it
-from pymongo import MongoClient
-from bson.objectid import ObjectId
 import random
-from alchemyData import AllEffects, AllIngredients, indexOn
 import json, os
 
+from madlibs import AllMadlibsByName
+from ingredient import AllIngredientsByName, get_ingredients_by_filter, get_ingredient_by_name
+from effect import AllEffectsByName, get_effect_by_name, get_effects_by_filter
+from potion import Potion
+
 __doc__ = """
-Create a mongodb client, usually as a docker container.
-
-C:> docker run -p 27017:27017 -d mongodb
-
 Then configure Flask.
 
 C:> set FLASK_APP=app
 C:> set FLASK_ENV=development
-
-Configure the data.
-
-C:> flask db_load
 
 Then you can start the service.  It will be at http://localhost:5000 by default.
 
 C:> flask run
 """
 
-conn = "mongodb://localhost:27017/"
-eng = MongoClient(conn)
-db = eng.alchemy
+# better place for these predicate functions?
+def pred_true(_):
+    return True
+
+def pred_and(*predicates):
+    return lambda x: all(predicate(x) for predicate in predicates)
+
+def pred_or(*predicates):
+    return lambda x: any(predicate(x) for predicate in predicates)
 
 app = Flask(__name__)
 
-# Good grief!  flask.jsonify doesn't support ObjectId?!
-class JSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        return json.JSONEncoder.default(self, o)
-
-# No thanks, flask.  I've got this.
-def jsonify(data):
-    return JSONEncoder().encode(data)
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+
 @app.route("/")
 def home():
     return render_template('index.html')
+
 
 @app.route("/skyrim")
 def skyrim():
     return render_template('skyrim.html')
 
+
 @app.route("/skyrim/ingredients")
-def skyrimIngredients():
-    filter = {}
+def skyrim_ingredients():
     if 'farmable' in request.args:
         farmable = { "True":True, "true":True }.get(request.args.get("farmable"), False)
-        filter['farmable'] = farmable
-    sort = ('value', -1)
-    if 'sortby' in request.args:
-        sort = (request.args.get('sortby'), 1)
-    ingredients = list(db.ingredients.find(filter, sort=[sort]))
-    buffs = { i['name']: hasBuffs(i) for i in ingredients }
-    nerfs = { i['name']: hasNerfs(i) for i in ingredients }
-    return render_template('ingredients.html', ingredients=ingredients, buffs=buffs, nerfs=nerfs)
+        predicate = lambda ingredient: ingredient.farmable == farmable
+    else:
+        predicate = pred_true
+    # sort = { 'field': 'value', 'reverse': True }
+    # if 'sortby' in request.args:
+    #     sort = { 'field': request.args.get('sortby'), 'reverse': False }
+    ingredients = get_ingredients_by_filter(predicate)
+    #ingredients.sort(key=lambda ingredient: ingredient[sort['field']], reverse=sort['reverse'])
+    return render_template('ingredients.html', ingredients=ingredients)
 
-def hasBuffs(ingredient):
-    return any(effect['power'] > 1 for effect in ingredient['effects']) or any(effect['value'] > 1 for effect in ingredient['effects'])
-
-def hasNerfs(ingredient):
-    return any(effect['power']< 1 for effect in ingredient['effects']) or any(effect['value'] < 1 for effect in ingredient['effects'])
 
 @app.route("/skyrim/ingredients/<name>")
-def skyrimIngredient(name):
-    item = db.ingredients.find_one({ 'name': name })
+def skyrim_ingredient(name):
+    item = get_ingredient_by_name(name)
     if item:
         return render_template('ingredient.html', ingredient=item)
     else:
         return "Not Found", 404
 
+
 @app.route("/skyrim/effects")
-def skyrimEffects():
-    filter = {}
+def skyrim_effects():
     if 'school' in request.args:
         school = request.args.get('school')
-        filter['school'] = school
+        match_school = lambda effect: effect.school == school
+    else:
+        match_school = pred_true
     if 'type' in request.args:
         effect_type = request.args.get('type')
-        filter['type'] = effect_type
-    return render_template('effects.html', effects=db.effects.find(filter, sort=[('cost', -1)]))
+        match_type = lambda effect: effect.type == effect_type
+    else:
+        match_type = pred_true
+    effects = get_effects_by_filter(pred_and(match_school, match_type))
+    #effects.sort(key=lambda effect: effect.cost, reverse=True)
+    return render_template('effects.html', effects=effects)
+
 
 @app.route("/skyrim/effects/<name>")
-def skyrimEffect(name):
-    item = db.effects.find_one({ 'name': name })
+def skyrim_effect(name):
+    item = get_effect_by_name(name)
     if item:
         return render_template('effect.html', effect=item)
     else:
         return "Not Found", 404
 
+
 @app.route('/api/skyrim/potions', methods=['GET'])
-def skyrimPotionsApi():
+def skyrim_potions_api():
     ingredients = request.args.get('ingredients').split(',')
-    ingredientsByName = indexOn('name', db.ingredients.find())
-    effectsByName = indexOn('name', db.effects.find())
-    potions = getPotions(ingredients, ingredientsByName, effectsByName)
-    return jsonify({'potions': potions, 'ingredients': ingredients})
+    potions = Potion.brew(ingredients)
+    return json.dumps({'potions': potions, 'ingredients': ingredients})
+
 
 @app.route('/skyrim/potions')
-def skyrimPotions():
-    def getIngredients(ingredientsByName):
+def skyrim_potions():
+    def getIngredients():
         if 'ingredients' in request.args:
             ingredients = request.args.get('ingredients')
-            if ingredients == "all": return list(ingredientsByName.keys())
-            if ingredients == "farmable": return [ ingredient["name"] for ingredient in ingredientsByName.values() if ingredient["farmable"] ]
+            if ingredients == "all": return list(AllIngredientsByName.keys())
+            if ingredients == "farmable": return [ ingredient.name for ingredient in AllIngredientsByName.values() if ingredient.farmable]
             return ingredients.split(',')
         else:
-            ingredients = list(ingredientsByName.keys())
+            ingredients = list(AllIngredientsByName.keys())
             random.shuffle(ingredients)
             return ingredients[:5]
     limit = 100
-    ingredientsByName = indexOn('name', db.ingredients.find())
-    effectsByName = indexOn('name', db.effects.find())
-    ingredients = getIngredients(ingredientsByName)
-    potions = getPotions(ingredients, ingredientsByName, effectsByName)
+    ingredients = getIngredients()
+    potions = Potion.brew(ingredients)
     return render_template('potions.html', potions=potions[:limit], ingredients=ingredients)
 
-AllMadlibs = {
-    'xkcd Calendar Facts': calendar_facts,
-    "Country Song": country_song,
-    "Alt Text": calendar_altText,
-    "Whip It": whip_it
-}
-
-MadlibRefs = {
-    'xkcd Calendar Facts': 'https://xkcd.com/1930/'
-}
 
 @app.route("/madlibs")
 def all_madlibs():
-    return render_template('madlibs.html', names=AllMadlibs.keys())
+    return render_template('madlibs.html', names=AllMadlibsByName.keys())
+
 
 @app.route("/madlibs/<name>")
 def madlibs(name):
-    if name in AllMadlibs:
-        return render_template('madlib.html', name=name, lines=madlib(AllMadlibs[name]), ref=MadlibRefs.get(name, None))
+    if name in AllMadlibsByName:
+        item = AllMadlibsByName[name]
+        return render_template('madlib.html', name=name, lines=item.render(), ref=item.ref)
     return "Not Found", 404
 
-@app.cli.command('db_load')
-def loadDb():
-    db.ingredients.insert_many(AllIngredients.values())
-    db.effects.insert_many(AllEffects.values())
-    # could also create indexes
-    #db.ingredients.createIndex({'name':1}, {'unique': True})
-    #db.ingredients.createIndex({'farmable':1})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
