4d6e7f3fc12f7040fbbd9b1005ede461cf7d2965
James Fordemwalt <jfordemwalt@clearwateranalytics.com>
2025-03-20 15:10:57 -0600

    Better docker container control.

--- tasks.py
+++ tasks.py
@@ -16,9 +16,18 @@ def docker_build(c):
     c.run('docker build -t alchemy .')
 
 @task
+def docker_stop(c):
+    "invoke docker-stop"
+    docker_id = c.run('docker ps -q --filter "name=alchemy"', hide=True).stdout
+    if docker_id:
+        c.run(f'docker stop {docker_id}')
+        c.run(f'docker rm {docker_id}')
+
+@task
 def docker_run(c):
     "invoke docker-run"
-    c.run('docker run -p 8088:8088 -d alchemy')
+    docker_stop(c)
+    c.run('docker run --name alchemy -p 8088:8088 -d alchemy')
 
 @task
 def docker_push(c):
